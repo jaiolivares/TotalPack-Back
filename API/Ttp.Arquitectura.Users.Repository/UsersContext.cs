777a19e3496757c97953dd09a32a8c3061406e2f
Javier Olivares Zavala <javier.olivares.zavala@gmail.com>
2024-06-17 15:37:06 -0400

    Carga inicial TotalPack-Back

---
+++ API/Ttp.Arquitectura.Users.Repository/UsersContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ttp.Arquitectura.Users.Domain;
+
+namespace Ttp.Arquitectura.Users.Repository
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
---
+++ API/Ttp.Arquitectura.Users.WebApi/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Ttp.Arquitectura.Users.Application.Commands;
+using Ttp.Arquitectura.Users.Application.Queries;
+using Ttp.Arquitectura.Users.WebApi.Models.Request;
+using Ttp.Arquitectura.Users.WebApi.Models.Response;
+
+namespace Ttp.Arquitectura.Users.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(AddUserHandler addUserHandler) : ControllerBase
+    {
+        private readonly AddUserHandler _addUserHandler = addUserHandler;
+        private readonly GetUsersHandler _getUsersHandler;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_getUsersHandler.Handle().Adapt<List<GetUserResponse>>());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AddUserRequest request)
+        {
+            _addUserHandler.Handle(request.Adapt<AddUserCommand>());
+            return Ok();
+        }
+    }
+}
